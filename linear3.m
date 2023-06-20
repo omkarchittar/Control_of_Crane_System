dfba0ac4b5abb1f2e34b628fa888941eb712748f
Andy <A@gmail>
2023-06-21 00:29:18 +0530

    first fork pratice

--- linear3.m
+++ linear3.m
@@ -10,5 +10,6 @@ B = [0; 1/M; 0; 1/(L1*M); 0; 1/(L2*M)];
 c4 = [1 0 0 0 0 0; 0 0 1 0 0 0; 0 0 0 0 1 0];
 y4 = [Qe(1); Qe(3); Qe(5)];
 K = 1; % feedback = 1;
+#practice
 dQe = (A+B*K)*Qe + Lue4*(y4 - c4*Qe);
 end
